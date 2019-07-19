f6d40aa2da54eea38ae43305e9d0f56d91c2fa51
YeonGyu <code.yeon.gyu@gmail.com>
2019-07-19 14:32:41 +0900

    Create a Django project
    with apps called community, resume, school_record, chat, school_recommend

---
+++ backend/school_recommend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SchoolRecommendConfig(AppConfig):
+    name = 'school_recommend'
---
+++ backend/school_record/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SchoolRecordConfig(AppConfig):
+    name = 'school_record'
